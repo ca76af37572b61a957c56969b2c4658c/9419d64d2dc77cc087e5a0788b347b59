ecd32b3d5b332d2111580c9926e280218d2c5534
ca76af37572b61a957c56969b2c4658c <92020889+ca76af37572b61a957c56969b2c4658c@users.noreply.github.com>
2021-12-07 13:02:52 -0800

    Delete ica0002-lab10 directory

--- ica0002-lab10/roles/influxdb_backup/files/infuxdb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-influxd backup -database telegraf /home/backup/backup/influxdb
-rm -rf /home/backup/influxdb/*; influxd backup -portable -database telegraf /home/backup/influxdb
-
--- ica0002-lab10/roles/mysql_backup/files/mysql.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mysqldump agama > agama.sql
-scp agama.sql bematv@backup:
-duplicity --no-encryption full /home/backup/mysql/ rsync://bematv@backup.pho.ng//home/bematv/
